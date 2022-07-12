c967a9a2de221b4a9ff0e485430fa3e009194e5b
Karine <conchal.karine@gmail.com>
2022-07-11 21:31:08 -0300

    create ex008
    executado com node.js

---
+++ aula 11/exer008.js
@@ -0,0 +1,10 @@
+var vel = 80
+
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+if (vel > 60) {
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+else {
+  console.log(`Mantenha a velocidade!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
