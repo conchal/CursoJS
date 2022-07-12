5fb036469856c02624d255ddfeb13f7c71817176
Karine <conchal.karine@gmail.com>
2022-07-11 21:36:07 -0300

    add "Condição Simples"

--- aula 11/exer008.js
+++ aula 11/exer008.js
@@ -1,10 +1,8 @@
 var vel = 80
 
 console.log(`A velocidade do seu carro é ${vel} KM/h`)
-if (vel > 60) {
+if (vel > 60) // Condição Simples!
+{
   console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
-}
-else {
-  console.log(`Mantenha a velocidade!`)
 }
 console.log(`Dirija sempre usando cinto de segurança!`)
