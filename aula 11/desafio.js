be8537117a8efd1a88883fbb7fa632ba51a365ba
Karine <conchal.karine@gmail.com>
2022-07-13 15:13:01 -0300

    Create desafio.js
    great

---
+++ aula 11/desafio.js
@@ -0,0 +1,12 @@
+var sem = 'Vermelho'
+
+console.log(`Semáfaro está: ${sem}`)
+if (sem == 'Verde') // Condição Simples!
+{
+  console.log(`PROSSIGA!`)
+}
+else {
+  console.log(`PARE!`)
+}
+
+console.log(`Dirija com atenção!!!`)
