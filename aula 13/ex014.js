0f1bdf50031d6b1ae7a1fc01632553d4423205af
Karine <conchal.karine@gmail.com>
2022-07-26 09:10:47 -0300

    Update ec014

--- aula 13/ex014.js
+++ aula 13/ex014.js
@@ -1,15 +1,18 @@
-var cont = 1
+/* OPÇÃO 1 */
 
+var cont = 1
 do {
   console.log(`Passo ${cont}`)
   cont++
 }
 while (cont <= 10) 
- 
 
 
-/* var cont = 1
+
+/* OPÇÃO 2 */
+
+ var cont = 1
 while (cont <= 10) {
   console.log(`Passo ${cont}`)
   cont++
-} */
+} 
