1dccc7cdff79897f695dacdb679bd8dc0fd8e65a
Karine <conchal.karine@gmail.com>
2022-07-26 09:32:33 -0300

    Update

--- aula 13/ex014.js
+++ aula 13/ex014.js
@@ -11,7 +11,7 @@ while (cont <= 10)
 
 /* OPÇÃO 2 */
 
- var cont = 1
+var cont = 1
 while (cont <= 10) {
   console.log(`Passo ${cont}`)
   cont++
--- aula 14/ambiente.js
+++ aula 14/ambiente.js
@@ -1,11 +1,13 @@
+/* OPÇÃO 1 */
 
-/*
 var cont = 1
 while (cont <= 10) {
   console.log(`Passo ${cont}`)
   cont++
 }
-*/
+
+
+/* OPÇÃO 2 */
 
 console.log('Vai começar...')
 
