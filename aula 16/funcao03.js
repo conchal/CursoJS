5405ec43ba972e7f8f3141e135ec8507bc31623a
Karine <conchal.karine@gmail.com>
2022-08-08 07:20:48 -0300

    Update funcao03

--- aula 16/funcao03.js
+++ aula 16/funcao03.js
@@ -1,5 +1,7 @@
-let v = function() {
+let v = function(x) {
   return x*2
 }
 
-console.log(v(5))
+console.log(v(5)) 
+
+//eu passei pra minha variável (v) um parâmetro (5) e ele conseguiu executar uma função (x*2)
