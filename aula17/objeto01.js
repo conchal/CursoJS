b49750b218bfc425dda55af7e77e9b27789f7353
Karine <conchal.karine@gmail.com>
2022-08-09 09:49:04 -0300

    Update objeto.js

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -1,10 +1,12 @@
-let amigo = {nome: 'Jose',
+let amigo = {
+nome: 'Jose',
 sexo: 'M',
 peso: 85.4,
-engordar(p=0){
+engordar(p=0) {
   console.log('Engordou!')
   this.peso += p
-}}
+}
+  }
 
 amigo.engordar(2)
 console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
